435830109cf951fa11c870dca8bad7b08c80fea3
vince <vincent.grandiere@gmail.com>
2017-03-18 17:32:04 +0100

    SignUp: Update regex mail

--- src/components/auth/SignUp.react.js
+++ src/components/auth/SignUp.react.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const regex = {
   pseudo: /^[\w-]{3,24}$/,
   password: /^.{8,}$/,
-  email: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/,
+  email: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/,
 }
 regex.pseudo.description = 'between 3 and 24 characters, no special characters exept "_"'
 regex.password.description = 'minimum 8 characters'
