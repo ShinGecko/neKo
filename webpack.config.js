8d219e7d39f39e9c7594eddad8a1d02086987b7a
DESKTOP-EPCPNTK\Vince <vincent.grandiere@gmail.com>
2017-03-22 19:54:00 +0100

    Webpack: Now application use webpack
    * Use 'npm start' to build everytime you modify a file
    * User 'npm run build' to build

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+let path = require('path')
+let webpack = require('webpack')
+
+let BUILD_DIR = path.resolve(__dirname, 'src/build')
+let APP_DIR = path.resolve(__dirname, 'src')
+
+let config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css/,
+        include: APP_DIR,
+        loader: 'css-loader',
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'global.GENTLY': false })
+  ],
+  node: {
+    __dirname: true,
+  },
+}
+
+module.exports = config
