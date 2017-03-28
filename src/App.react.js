bba8e1f537c6446325ae6aba18e09ed6b13c7026
DESKTOP-EPCPNTK\Vince <vincent.grandiere@gmail.com>
2017-03-28 18:03:06 +0200

    auth: Auth does new works

--- src/App.react.js
+++ src/App.react.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import SignIn from './components/auth/SignIn.react'
 import SignUp from './components/auth/SignUp.react'
-
-const request = require('superagent')
+import { signIn, signUp } from './utils/auth'
 
 class App extends Component {
   constructor(props) {
@@ -12,36 +11,30 @@ class App extends Component {
     this.state = {
       submited: '',
     }
+
+    this.handleSubmitSignIn = this.handleSubmitSignIn.bind(this)
+    this.handleSubmitSignUp = this.handleSubmitSignUp.bind(this)
+  }
+
+  handleSubmitSignIn(login, password) {
+    this.setState({ submited: 'Sign In => login : ' + login + ', password : ' + password })
+
+    signIn(login, password)
+  }
+
+  handleSubmitSignUp(login, password, email) {
+    this.setState({ submited: 'Sign Up => login : ' + login + ', password : ' + password + ', email : ' + email })
+
+    signUp(login, password, email)
   }
 
   render() {
-    const signIn = (login, password) => {
-      this.setState({ submited: 'Sign In => login : ' + login + ', password : ' + password })
-    }
-
-    const signUp = (login, password, email) => {
-      this.setState({ submited: 'Sign Up => login : ' + login + ', password : ' + password + ', email : ' + email })
-
-      request
-        .post('http://localhost:8085')
-        .send({ login: 'testboy', email: 'test@test.test', password: 'test1234' })
-        .withCredentials()
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .end(function (err) {
-          if (err) {
-            console.log(err)
-          }
-          // Calling the end function will send the request
-        })
-    }
-
     return (
       <div className="app">
         <p> {this.state.submited} </p>
-        <SignIn onSubmit={signIn}/>
+        <SignIn onSubmit={this.handleSubmitSignIn}/>
         <hr/>
-        <SignUp onSubmit={signUp}/>
+        <SignUp onSubmit={this.handleSubmitSignUp}/>
       </div>
     )
   }
---
+++ src/utils/auth.js
@@ -0,0 +1,32 @@
+const request = require('superagent')
+
+
+const signIn = (login, password) => {
+  request
+    .post('http://localhost:8085/users/auth')
+    .send({ login, password })
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .end(function (err) {
+      if (err) {
+        console.log(err)
+      }
+        // Calling the end function will send the request
+    })
+}
+
+const signUp = (login, password, email) => {
+  request
+    .post('http://localhost:8085/users/create')
+    .send({ login, email, password })
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .end(function (err) {
+      if (err) {
+        console.log(err)
+      }
+        // Calling the end function will send the request
+    })
+}
+
+module.exports = { signIn, signUp }
