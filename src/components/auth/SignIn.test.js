92ff3aec0d268b9c55309f1af38fb89782fde103
vince <vincent.grandiere@gmail.com>
2017-04-09 09:46:48 +0200

    Test: Fixed

--- src/components/auth/SignIn.test.js
+++ src/components/auth/SignIn.test.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { signIn } from './../../utils/auth'
 import SignIn from './SignIn.react'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<SignIn/>, div)
+  ReactDOM.render(<SignIn onSubmit={signIn}/>, div)
 })
--- src/components/auth/SignUp.test.js
+++ src/components/auth/SignUp.test.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { signUp } from './../../utils/auth'
 import SignUp from './SignUp.react'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<SignUp/>, div)
+  ReactDOM.render(<SignUp onSubmit={signUp}/>, div)
 })
